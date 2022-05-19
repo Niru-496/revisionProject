70acac679a8e800a35b05d55ca70b9975c0bba47
Niranjan <narayana9492324464@gmail.com>
2022-05-19 16:28:45 +0530

    minor changes in Server

--- src/App.js
+++ src/App.js
@@ -8,11 +8,7 @@ import SingleProduct from "./screens/SingleProduct";
 import Login from "./screens/Login";
 import Register from "./screens/Register";
 import CartScreen from "./screens/CartScreen";
-import ShippingScreen from "./screens/ShippingScreen";
 import ProfileScreen from "./screens/ProfileScreen";
-import PaymentScreen from "./screens/PaymentScreen";
-import PlaceOrderScreen from "./screens/PlaceOrderScreen";
-import OrderScreen from "./screens/OrderScreen";
 import NotFound from "./screens/NotFound";
 
 const App = () => {
@@ -25,13 +21,10 @@ const App = () => {
 				<Route path="/register" element={<Register />} />
 				<Route path="/profile" element={<ProfileScreen />} />
 				<Route path={`/cart/:id`} element={<CartScreen />} />
-				<Route path="/shipping" element={<ShippingScreen />} />
-				<Route path="/payment" element={<PaymentScreen />} />
-				<Route path="/placeorder" element={<PlaceOrderScreen />} />
-				<Route path="/order" element={<OrderScreen />} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</div>
+
 	);
 };
 
